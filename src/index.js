e0c5deb919f9a3a40baf0bb1da06abb546f1f592
Tim Rooke <timrooke1991@gmail.com>
2021-05-24 08:03:39 +0100

    reducer wired up

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+import './styles.scss';
+
+const initialState = {
+  count: 0
+}
+
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
+const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1
+    }
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+class Counter extends Component {
+  render() {
+    return (
+      <main className="Counter">
+        <p className="count">0</p>
+        <section className="controls">
+          <button>Increment</button>
+          <button>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    );
+  }
+}
+
+render(<Counter />, document.getElementById('root'));
